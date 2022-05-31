226a6373272a719b081e15d45faf008ce275caa0
AlexeyG1991 <68025816+AlexeyG1991@users.noreply.github.com>
2022-05-31 23:54:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+        var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+        }
+        return false; // выключаем стандартное действие
+    });
+});
+
+$('.menu-btn').on('click', function(e) {
+  e.preventDefault();
+  $(this).toggleClass('menu-btn_active');
+  $('.menu-nav').toggleClass('menu-nav_active');
+});
+
+$(function(){
+    $('a[href^="#"]').on('click', function(event) {
+      // отменяем стандартное действие
+      event.preventDefault();
+      
+      var sc = $(this).attr("href"),
+          dn = $(sc).offset().top;
+      /*
+      * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+      * dn - определяем положение блока на странице
+      */
+      
+      $('html, body').animate({scrollTop: dn}, 1000);
+      
+      /*
+      * 1000 скорость перехода в миллисекундах
+      */
+    });
+  });
